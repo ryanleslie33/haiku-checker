62da58f00dbb5444150a97b4fcf508e0444ff6d5
Robert Lee and ryan leslie <ryan@>
2019-01-09 15:35:07 -0800

    complete regex code for functionality

--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -3,24 +3,24 @@ import {Haiku} from '/Users/Guest/desktop/haiku/src/haiku.js';
 describe("Haiku", function(){
   var haiku;
   beforeEach(function() {
-    haiku = new Haiku("the the the the the", "the the the the the the the", "the the the the the");
+    haiku = new Haiku("like fire cafe eat food fee this eat fee fee this this", "sentence sentence sentece this that the the the the the the the", " computer computer computer computer the the the the the");
 
   });
 
 it ( "determines whether line has <= 5 syllables", function(){
 
   console.log(haiku.syllableChecker1())
-  expect(haiku.syllableChecker1()).toBe(true)
+  expect(haiku.syllableChecker1()).toBe(false)
 })
 
 it ( "determines whether line has <= 7 syllables", function(){
   console.log(haiku.syllableChecker2())
-  expect(haiku.syllableChecker2()).toBe(true)
+  expect(haiku.syllableChecker2()).toBe(false)
 })
 
 it ( "determines whether line has <= 5 syllables", function(){
   console.log(haiku.syllableChecker3())
-  expect(haiku.syllableChecker3()).toBe(true)
+  expect(haiku.syllableChecker3()).toBe(false)
 })
 
 });
--- src/haiku.js
+++ src/haiku.js
@@ -6,21 +6,22 @@ export class Haiku {
   }
 
   //In English, a diphthong is one vowel sound represented by two letters of vowels
-  //const dipthong = ["ai", "ay", "ee", "ea", "ie", "ei", "oo", "ou", "ow", "oe", "oo", "ue", "ey", "ay", "oy", "oi", "au", "aw"];
-
-
+  // const dipthong = ["ai", "ay", "ee", "ea", "ie", "ei", "oo", "ou", "ow", "oe", "oo", "ue", "ey", "ay", "oy", "oi", "au", "aw"];
+//
+// ai|ay|ee|ea|ie|ei|oo|ou|ow|oe|oo|ue|ey|ay|oy|oi|au|aw
 
 
   syllableChecker1() {
-    let regex = /oo|y\b|[^aeiou]le\b|[^aeiou]les\b|[aeiou]|[^aeiou]e\b/g;
+    let regex = /ai|ay|ee|ea|ie|ei|oo|ou|ow|oe|oo|ue|ey|ay|oy|oi|au|iou|aw|y\b|[^aeiou]le\b|[^aeiou]les\b|[a-z]{2}e\b|[aeiou]/g;
     let found = this.line1.match(regex);
+    console.log(found)
     if (found.length < 6) {
       return true }
     return false;
   }
 
   syllableChecker2() {
-    let regex = /oo|y\b|[^aeiou]le\b|[^aeiou]les\b|[aeiou]|[^aeiou]e\b/g;
+    let regex = /ai|ay|ee|ea|ie|ei|oo|ou|ow|oe|oo|ue|ey|ay|oy|oi|au|aw|y\b|[^aeiou]le\b|[^aeiou]les\b|[aeiou]|[^aeiou]e\b/g;
     let found = this.line2.match(regex);
     if (found.length < 8) {
       return true }
@@ -28,7 +29,7 @@ export class Haiku {
     }
 
     syllableChecker3() {
-      let regex = /oo|y\b|[^aeiou]le\b|[^aeiou]les\b|[aeiou]|[^aeiou]e\b/g;
+      let regex = /ai|ay|ee|ea|ie|ei|oo|ou|ow|oe|oo|ue|ey|ay|oy|oi|au|aw|y\b|[^aeiou]le\b|[^aeiou]les\b|[aeiou]|[^aeiou]e\b/g;
       let found = this.line3.match(regex);
       if (found.length < 6) {
         return true }
