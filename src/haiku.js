95f06e7c9c1899e01f28417d106c418d05d337fb
Robert Lee and ryan leslie <ryan@>
2019-01-10 12:59:33 -0800

    add function for date finder

--- src/haiku.js
+++ src/haiku.js
@@ -6,10 +6,8 @@ export class Haiku {
   }
 
 
-
-
   syllableChecker1() {
-    let regex = /[aeiou]dge\b|eau|[cg]eous|[cgt]ious|qu[aeiou]{1,2}|[a-z]{2,3}es\b|[a-z]{2,3}e\b|ay|ee|ea|ie|ei|oo|ou|ow|oe|oo|ue|ey|ay|oy|oi|au|aw|y\b|[^aeiou]le\b|[^aeiou]les\b|ia|[aeiou]/g;
+    let regex = /[aeiou]dge\b|eau|[cg]eous|[cgt]ious|qu[aeiou]|ai|ay|ee|ea|ie|ei|oo|ou|ow|oe|oo|ue|ey|ay|oy|oi|au|aw|y\b|[^aeiou]le\b|[^aeiou]les\b|[a-z]{2}es\b|[a-z]{2}e\b|ia|[aeiou]/g;
     let found = this.line1.match(regex);
     console.log(found)
     if (found.length === 5) {
@@ -18,18 +16,18 @@ export class Haiku {
     }
 
   syllableChecker2() {
-    let regex = /[aeiou]dge\b|eau|[cg]eous|[cgt]ious|qu[aeiou]{1,2}|[a-z]{2,3}es\b|[a-z]{2,3}e\b|ay|ee|ea|ie|ei|oo|ou|ow|oe|oo|ue|ey|ay|oy|oi|au|aw|y\b|[^aeiou]le\b|[^aeiou]les\b|ia|[aeiou]/g;
+    let regex = /[aeiou]dge\b|eau|[cg]eous|[cgt]ious|qu[aeiou]|ai|ay|ee|ea|ie|ei|oo|ou|ow|oe|oo|ue|ey|ay|oy|oi|au|aw|y\b|[^aeiou]le\b|[^aeiou]les\b|[a-z]{2}es\b|[a-z]{2}e\b|ia|[aeiou]/g;
     let found = this.line2.match(regex);
-      console.log(found)
+    console.log(found)
     if (found.length === 7) {
       return true }
       return false;
     }
 
   syllableChecker3() {
-    let regex = /[aeiou]dge\b|eau|[cg]eous|[cgt]ious|qu[aeiou]{1,2}|[a-z]{2,3}es\b|[a-z]{2,3}e\b|ay|ee|ea|ie|ei|oo|ou|ow|oe|oo|ue|ey|ay|oy|oi|au|aw|y\b|[^aeiou]le\b|[^aeiou]les\b|ia|[aeiou]/g;
+    let regex = /[aeiou]dge\b|eau|[cg]eous|[cgt]ious|qu[aeiou]|ai|ay|ee|ea|ie|ei|oo|ou|ow|oe|oo|ue|ey|ay|oy|oi|au|aw|y\b|[^aeiou]le\b|[^aeiou]les\b|[a-z]{2}es\b|[a-z]{2}e\b|ia|[aeiou]/g;
     let found = this.line3.match(regex);
-      console.log(found)
+    console.log(found)
     if (found.length === 5) {
       return true }
       return false;
@@ -44,5 +42,96 @@ export class Haiku {
       return false;
     }
   }
+}
 
+
+export class Week {
+  constructor(year,month,day){
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+
+  checkDate() {
+    switch (this.month) {
+
+    case "JAN":
+    this.month = 0;
+    break;
+
+    case "FEB":
+    this.month = 1;
+    break;
+
+    case "MAR":
+    this.month = 2;
+    break;
+
+    case "APR":
+    this.month = 3;
+    break;
+
+    case "MAY":
+    this.month = 4;
+    break;
+
+    case "JUN":
+    this.month = 5;
+    break;
+
+    case "JUL":
+    this.month = 6;
+    break;
+
+    case "AUG":
+    this.month = 7;
+    break;
+
+    case "SEP":
+    this.month = 8;
+    break;
+
+    case "OCT":
+    this.month = 9;
+    break;
+
+    case "NOV":
+    this.month = 10;
+    break;
+
+    case "DEC":
+    this.month = 11;
+    }
+
+
+var output_day;
+
+let another_date_object = new Date(this.year,this.month,this.day);
+
+let weekday = another_date_object.getDay();  //This should be a number between 0 and 6
+
+    switch (weekday) {
+    case 0:
+    output_day = "Sunday";
+    break;
+    case 1:
+    output_day = "Monday";
+    break;
+    case 2:
+    output_day = "Tuesday";
+    break;
+    case 3:
+    output_day = "Wednesday";
+    break;
+    case 4:
+    output_day = "Thursday";
+    break;
+    case 5:
+    output_day = "Friday";
+    break;
+      case 6:
+      output_day = "Saturday";
+    }
+    return output_day;
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,22 +1,32 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
-import { Haiku } from './haiku.js';
+import { Haiku, Week } from './haiku.js';
 
 
 
-
+const userTime = new Date()
 
 $(document).ready(function() {
 $("#form").submit(function(event){
 event.preventDefault()
   let mainline1 = $("#line1").val();
-  console.log(mainline1)
   let mainline2 = $("#line2").val();
   let mainline3 = $("#line3").val();
   let haiku = new Haiku(mainline1,mainline2,mainline3);
-  console.log(haiku.isAHaiku());
-  $("#output").text("its a haiku poem" + haiku.line1 + haiku.line2 + haiku.line3);
+
+  $("#output").html("<p>its a haiku poem! </p><p>" + haiku.line1 + ", </p><p>"  + haiku.line2 + ", </p><p>"  + haiku.line3 +  ", </p><p>"   + "Time: </p>" + userTime + "</p>");
+});
+  $("#form2").submit(function(event){
+  event.preventDefault()
+  let year = $("#year").val();
+  let month = $("#month").val();
+  let day = $("#day").val();
+  console.log("where is day" + day)
+  let dayOfWeek = new Week(year,month,day);
+  console.log("where is day" + dayOfWeek.day)
+
+  $("#output2").text(dayOfWeek.checkDate());
 
 })
 
